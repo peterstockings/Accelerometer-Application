ea1dc22411dab6b1ee0fa660727c1526f3c00722
peterstockings <peterstockings@gmail.com>
2014-08-07 00:20:12 +1000

    plots 3 data sets at once

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ var freqDiff = 100;
 var plotUpdateDiff = 100;
 var dataPointDiff = 5;
 
+//X padding for plot
+var xPadding = 5;
+var yPadding = 10;
+
 //Functions to change the Frequency of data aquisition
 function increaseFrequency() {
     document.getElementById("frequencyInput").value = parseInt(document.getElementById("frequencyInput").value.slice(0, -2)) + freqDiff + "ms";
@@ -50,81 +54,73 @@ function addDataToTable(timeStamp, Xacceleration, Yacceleration, Zacceleration) 
     table.appendChild(row);
 }
 
-/* graphing data might look something like this... 
-  var data = {
-    plot: [0,10,-10,20,-11,5,-20,11,-3]
-  };
-  */
 
-/* in this example we'll generate our test data 
-     randomly by calling generateTestData() which 
-     will return a random length array containing 
-     random values between |y| and -|y|
-  */
 
 function generateTestData() {
     // generate random graphing data for our test
     var x = Math.floor((Math.random() * 50) + 10);
     var data = [];
     for (var i = 0; i < x; i++) {
-        data[i] = {};
-        data[i].date = i;
         var tmp = Math.floor((Math.random() * 20) + 1) - 10;
-        data[i].plot = (tmp > 30) ? 30 : tmp; /* aiming for somewhere between -y and y */
+        data[i] = (tmp > 30) ? 30 : tmp; /* aiming for somewhere between -y and y */
     }
     return data;
 }
 
-function clearCanvas(context, canvas) {
+function clearCanvas() {
+    var canvas = document.getElementById('chart');
+    var context = canvas.getContext('2d');
     context.clearRect(0, 0, canvas.width, canvas.height);
     var w = canvas.width;
     canvas.width = 1;
     canvas.width = w;
 }
 
-function plotGraph(args) {
-    var chartEl = args['chartEl'];
-    var data = args['data'];
+function plotGraph() {
+    var dataX = generateTestData();
+    var dataY = generateTestData();
+    var dataZ = generateTestData();
 
-    var canvas = document.getElementById(chartEl);
+    var tempsX = [50, 95, 85, 78, 92, 95, 79, 10, 5, 3, 4, 2, 1, 0, 100];
+    var tempsY = [70, 20, 55, 45, 45, 65, 90, 5];
+    var tempsZ = [20, 30, 40, 20, 30, 40];
+
+    var canvas = document.getElementById('chart');
     var context = canvas.getContext('2d');
-    if (canvas && canvas.getContext) {
-
-        var x = 10; /* start inside the grid so we can show the line endings */
-        y = (canvas.height / 2);
-        clearCanvas(context, canvas);
-        context.beginPath();
-        context.moveTo(x, y);
-
-        var l = data.length;
-        if (!l) return; /* nothing to plot */
-        var xx = (canvas.width - x * 2) / l; /* scale horizontal plot based on the length of the data */
-        /* note: canvas.width-x*2 places the end of the line the same distance from the right 
-            hand-limit as the starting point of x...so we can see the line cap. */
-
-        var str = '';
-        for (var i = 0; i < l; i++) {
-            var yy = data[i].plot;
-            x += xx;
-            y += yy;
-            context.lineTo(x, y);
-
-            /* collect the data so we can show the user what we're plotting */
-            str += '' + (i > 0 ? ',' : '') + data[i].plot
-        }
-
-        /* pick a random color and line width for the graph plotter */
-        var colrs = ['#ff0000', '#00ff00', '#0000ff', 'rgb(50,50,50)', 'rgb(200,200,200)', 'purple', 'orange', 'black'];
-        var colr = colrs[Math.floor(Math.random() * colrs.length)];
-        var w = Math.floor(Math.random() * 10) + 1;
-
-        context.lineCap = 'round';
-        context.lineWidth = w;
-        context.strokeStyle = colr;
+    HEIGHT = canvas.height;
+    WIDTH = canvas.width;
+    context.lineCap = 'round';
+    context.lineWidth = 2;
+    context.strokeStyle = "red";
+    context.beginPath();
+    context.moveTo(0, HEIGHT - (tempsX[0]));
+    j = 0;
+    xDiffPlot = Math.floor(WIDTH / tempsX.length);
+    for (var i in tempsX) {
+        context.lineTo(j * xDiffPlot, HEIGHT - (tempsX[j]));
         context.stroke();
-
-    } else {
-        /* unsupported browser */
-        alert("Couldn't get a reference to the HTML 5 canvas. Your browser doesn't appear to support this page.");
+        j++;
     }
-};
+
+    context.beginPath();
+    context.strokeStyle = "green";
+    context.moveTo(0, HEIGHT - (tempsY[0]));
+    j = 0;
+    xDiffPlot = Math.floor(WIDTH / tempsY.length);
+    for (var i in tempsY) {
+        context.lineTo(j * xDiffPlot, HEIGHT - (tempsY[j]));
+        context.stroke();
+        j++;
+    }
+
+    context.beginPath();
+    context.strokeStyle = "blue";
+    context.moveTo(0, HEIGHT - (tempsZ[0]));
+    j = 0;
+    xDiffPlot = Math.floor(WIDTH / tempsZ.length);
+    for (var i in tempsZ) {
+        context.lineTo(j * xDiffPlot, HEIGHT - (tempsZ[j]));
+        context.stroke();
+        j++;
+    }
+}
